f9d865b9c63055048cf6b37448a0049ec2034ab3
filipeluiz <ffilipeluiz@gmail.com>
2021-02-26 18:13:42 -0300

    Ajustes next_config e getignore

--- next_config.js
+++ next_config.js
@@ -1,4 +1,14 @@
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
 module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/testeCognitivo': { page: '/testeCognitivo' },
+    '/adm/importDiscipline': { page: '/adm/importDiscipline' },
+    '/adm/importStudent': { page: '/adm/importStudent' },
+  }),
+  basePath: isProd ? 'automate-par2dnqsd-filipeluiz.vercel.app' : '',
+  assetPrefix: isProd ? 'automate-par2dnqsd-filipeluiz.vercel.app' : '',  
   images: {
     loader: 'imgix'
   }
