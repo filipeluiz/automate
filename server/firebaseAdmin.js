f42fda453258229e0085023b2b61e62c49325407
filipeluiz <ffilipeluiz@gmail.com>
2021-02-27 18:28:59 -0300

    Ajustes firebase admin para evite o bloqueio

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -6,16 +6,16 @@ export const verifyIdToken = (token) => {
   if(!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        type: process.env.NEXT_PUBLIC_SECRET_TYPE,
-        project_id: process.env.NEXT_PUBLIC_SECRET_PROJECT_ID,
-        private_key_id: process.env.NEXT_PUBLIC_SECRET_PRIVATE_KEY_ID,
-        private_key: process.env.NEXT_PUBLIC_SECRET_PRIVATE_KEY,
-        client_email: process.env.NEXT_PUBLIC_SECRET_CLIENT_EMAIL,
-        client_id: process.env.NEXT_PUBLIC_SECRET_CLIENT_ID,
-        auth_uri: process.env.NEXT_PUBLIC_SECRET_AUTH_URI,
-        token_uri: process.env.NEXT_PUBLIC_SECRET_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.NEXT_PUBLIC_SECRET_AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.NEXT_PUBLIC_SECRET_CLIENT_X509_CERT_URL
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY,
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
       }),
       databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
     })
