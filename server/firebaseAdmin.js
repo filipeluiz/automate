607353b6b56d5e9186a59d05e485779892b5102e
filipeluiz <ffilipeluiz@gmail.com>
2021-02-28 11:25:07 -0300

    add CNAME

--- server/firebaseAdmin.js
+++ server/firebaseAdmin.js
@@ -7,7 +7,7 @@ export const verifyIdToken = (token) => {
     admin.initializeApp({
       credential: admin.credential.cert({
         type: process.env.FIREBASE_TYPE,
-        project_id: process.env.NEXT_PUBLIC_PROJECT_ID,
+        project_id: process.env.FIREBASE_PROJECT_ID,
         private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
         private_key: process.env.FIREBASE_PRIVATE_KEY,
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
