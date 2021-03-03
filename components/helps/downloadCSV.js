faad66510467fa4c2bfe62aa46ea7af1ce2482c4
Filipe Luiz <ffilipeluiz@gmail.com>
2021-03-03 12:45:43 -0300

    Deixei apenas type csv

--- components/helps/downloadCSV.js
+++ components/helps/downloadCSV.js
@@ -3,7 +3,7 @@
 const downloadCSV = (data, fileName) => {
   const finalFileName = fileName.endsWith(".csv") ? fileName : `${fileName}.csv`
   const a = document.createElement("a")
-  a.href = URL.createObjectURL(new Blob([data], { type: "text/csv;charset=utf-8" }))
+  a.href = URL.createObjectURL(new Blob([data], { type: "text/csv" }))
   a.setAttribute("download", finalFileName)
   document.body.appendChild(a)
   a.click()
